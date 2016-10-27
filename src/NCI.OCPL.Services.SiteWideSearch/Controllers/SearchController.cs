30150aa470b21e13cdb2404777a23194abd31697
Bryan Pizzillo <bryanp@mail.nih.gov>
2016-10-27 18:23:55 -0400

    Updated methods to match our design. Also added comments. NOTE: This will break unit tests for now - they will be fixed in a bit.

--- src/NCI.OCPL.Services.SiteWideSearch/Controllers/SearchController.cs
+++ src/NCI.OCPL.Services.SiteWideSearch/Controllers/SearchController.cs
@@ -10,37 +10,73 @@ using Nest;
 
 namespace NCI.OCPL.Services.SiteWideSearch.Controllers
 {
+    /// <summary>
+    /// This controller handles requests for performaning site wide searches.
+    /// </summary>
     [Route("[controller]")]
     public class SearchController : Controller
     {
         private readonly IElasticClient _elasticClient;
         private readonly ILogger<SearchController> _logger;
 
+        /// <summary>
+        /// Creates a new instance of a Search Controller
+        /// </summary>
+        /// <param name="elasticClient">An instance of an IElasticClient to use for connecting to the ElasticSearch cluster</param>
+        /// <param name="logger">An instance of a ILogger to use for logging messages</param>
         public SearchController(IElasticClient elasticClient, ILogger<SearchController> logger)
         {
             _elasticClient = elasticClient;
             _logger = logger;
         }
 
-        // GET api/values/5
+        // GET search/cgov_en/lung+cancer
+        /// <summary>
+        /// Gets the results of a search
+        /// </summary>
+        /// <param name="collection">The search collection/strategy to use.  This defines the ES template to use.</param>
+        /// <param name="term">The search term to search for</param>
+        /// <param name="pagenum">The results page to retrieve</param>
+        /// <param name="numperpage">The number of items to retrieve per page</param>
+        /// <param name="site">An optional parameter used to limit the number of items returned based on site.</param>
+        /// <returns>A SiteWideSearchResults collection object</returns>
         [HttpGet("{term}")]
-        public SiteWideSearchResults Get(string term, int pagenum = 1)
+        public SiteWideSearchResults Get(
+            string collection, 
+            string term,             
+            [FromQuery] int from = 0,
+            [FromQuery] int size = 10,
+            [FromQuery] string site = "all" 
+            )
         {
 
-            _logger.LogInformation("Search Request -- Term: {0}, Page{1} ", term, pagenum);
-            
+            if (string.IsNullOrWhiteSpace(collection))
+                throw new APIErrorException(400, "You must supply a collection name and term");
+
+            if (string.IsNullOrWhiteSpace(term))
+                throw new APIErrorException(400, "You must supply a search term");
+
+            //TODO: Access Logging with params
+            //_logger.LogInformation("Search Request -- Term: {0}, Page{1} ", term, pagenum);
+
+            //Setup our template name based on the collection name
+            string templateName = "cgov_" + collection;
+
+            //TODO: Make this a parameter that can take in a list of fields and turn them
+            //into this string.
+            // Setup the list of fields we want ES to return.
+            string fields = "\"id\", \"url\", \"metatag-description\", \"metatag-dcterms-type\"";            
+
             //thios Can throw exception
             var response = _elasticClient.SearchTemplate<SiteWideSearchResult>(sd => sd
                 .Index("cgov")
-                .File("cgov_cgovSearch")
+                .File(templateName)
                 .Params(pd => pd
                     .Add("my_value", term)
-                    .Add("my_size", 10)
-                    .Add("my_from", 0)
-//                    .Add("my_fields", new string[]{
-//                        "id", "url", "metatag-description", "metatag-dcterms-type"
-//                    })
-                    .Add("my_site", "all")
+                    .Add("my_size", size)
+                    .Add("my_from", from)
+                    .Add("my_fields", fields)
+                    .Add("my_site", site)
                 )
             );   
 
@@ -52,8 +88,7 @@ namespace NCI.OCPL.Services.SiteWideSearch.Controllers
 
             } else {
                 throw new APIErrorException(500, "Error connecting to search servers");
-            }
-
+            }            
         }
 
     }
