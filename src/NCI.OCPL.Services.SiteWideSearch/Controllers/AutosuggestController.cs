d4956ef17bb7fa831cdf401ad23a833ff368a497
Bryan Pizzillo <bryanp@mail.nih.gov>
2016-10-27 18:31:34 -0400

    Added non-functional stub for autosuggestions.

---
+++ src/NCI.OCPL.Services.SiteWideSearch/Controllers/AutosuggestController.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+using Nest;
+
+namespace NCI.OCPL.Services.SiteWideSearch.Controllers
+{
+    /// <summary>
+    /// A Controller for hanlding requests for SiteWideSearch Suggestions
+    /// </summary>
+    [Route("[controller]")]
+    public class AutosuggestController : Controller
+    {
+        private readonly IElasticClient _elasticClient;
+        private readonly ILogger<SearchController> _logger;
+
+        public AutosuggestController(IElasticClient elasticClient, ILogger<SearchController> logger)
+        {
+            _elasticClient = elasticClient;
+            _logger = logger;
+        }
+
+        [HttpGet("{term}")]
+        public SiteWideSearchResults Get(
+            string language,
+            string term,
+            [FromQuery] int from = 0, //Really?  I mean, when do you page autosuggestions?
+            [FromQuery] int size = 10 
+            )
+        {
+            if (string.IsNullOrWhiteSpace(language))
+                throw new APIErrorException(400, "You must supply a language and term");
+
+            //TODO: Validate language
+
+            if (string.IsNullOrWhiteSpace(term))
+                throw new APIErrorException(400, "You must supply a search term");
+
+            
+
+            //TODO: Catch Exception
+            //TODO: Return List<Suggestion>            
+            var response = _elasticClient.SearchTemplate<SiteWideSearchResult>(sd => sd
+                .Index("cgov")
+                .File("cgov_sitewideAutosuggest")
+                .Params(pd => pd
+                    .Add("is_spanish", "true")
+                    .Add("my_size", 10)
+                    .Add("my_from", 0)
+                )
+            );   
+
+            if (response.IsValid) {
+                return new SiteWideSearchResults(
+                    response.Total,
+                    response.Documents
+                );
+
+            } else {
+                throw new APIErrorException(500, "Error connecting to search servers");
+            }            
+        }
+
+
+    }
+}
