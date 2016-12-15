e54086edd70d865a3420621244d7c1580e9cb292
Blair Learn <blairlearn@users.noreply.github.com>
2016-12-15 15:12:58 -0500

    Initial build scripts.

---
+++ scripts/build-system/release-to-prod.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# This is the template for releasing a build to production. 
+
+echo Doing a release to production. Woot!
---
+++ scripts/build-system/release-to-stage.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# This is the template for releasing a build to the staging tier. 
+
+echo Doing a release to staging.
