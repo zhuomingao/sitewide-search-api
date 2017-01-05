16bc2b43223f52874976b1bc65b851ad14855b3a
Blair Learn <blairlearn@users.noreply.github.com>
2017-01-05 16:17:44 -0500

    cleanup

--- tools/jenkins/release-to-prod.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# This is the template for releasing a build to production. 
-
-echo Doing a release to production. Woot!
--- tools/jenkins/release-to-stage.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# This is the template for releasing a build to the staging tier. 
-
-echo Doing a release to staging.
